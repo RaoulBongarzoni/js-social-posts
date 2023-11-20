4ceaf8423d243561a22e305a0ac1d6df9164155a
RaoulBongarzoni <raoul.bongarzoni99@gmail.com>
2023-11-20 16:03:46 +0100

    feat: post js generation

--- main.js
+++ main.js
@@ -55,3 +55,51 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+posts.forEach(element => {
+
+
+    if (element.author.image == null){
+        element.author.image ="https://unsplash.it/600/400?image=29" ;
+    }
+    document.getElementById("container").innerHTML += `
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src=${element.author.image} alt=${element.author.name}>                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${element.author.name}</div>
+                        <div class="post-meta__time">${element.created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${element.content}</div>
+            <div class="post__image">
+                <img src=${element.media} alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-${element.id}" class="js-likes-counter">${element.likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div> 
+    `
+/*     document.getElementById(`like-counter-${element.id}`).addEventListener("toggle", function(){
+        this.element.classList.add("like-button--liked");
+        
+
+    }) */
+    
+});
+
